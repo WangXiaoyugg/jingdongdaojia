1001c5ec750435096f87c30111cb0ac19dec05bf
xywang71 <xywang71@iflytek.com>
2021-01-26 15:37:26 +0800

    feat: 首页地址区域布局和样式

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,13 @@
 <template>
+  <div class="wrapper">
+    <div class="position">
+      <span class="iconfont position__icon">&#xe619;</span>
+      北京理工大学国防科技园2号楼10层
+      <span class="iconfont position__notice">&#xe60b;</span>
+    </div>
+  </div>
   <div class="docker">
-    <div class="docker__item">
+    <div class="docker__item docker__item--active">
       <div class="iconfont">&#xe6f3;</div>
       <div class="docker__title">首页</div>
     </div>
@@ -20,6 +27,34 @@
 </template>
 
 <style lang="scss">
+@import "./style/variables.scss";
+@import './style/mixins.scss';
+.wrapper {
+  position: absolute;
+  left: 0;
+  right: 0;
+  top: 0;
+  bottom: .5rem;
+  padding: 0 .18rem;
+}
+.position {
+  position: relative;
+  padding: .16rem .24rem .16rem 0;
+  line-height: .22rem;
+  font-size: .16rem;
+  color: $content-font-color;
+  @include ellipsis;
+  .position__icon {
+    position: relative;
+    top: .015rem;
+    font-size: .2rem;
+  }
+  .position__notice {
+    position: absolute;
+    right: 0;
+    top: 0.17rem;
+  }
+}
 .docker {
   display: flex;
   position: absolute;
@@ -29,22 +64,25 @@
   bottom: 0;
   width: 100%;
   height: .49rem;
-  border-top: 1px solid #f1f1f1;
-}
-.docker__item {
-  flex: 1;
-  text-align: center;
-  .iconfont {
-    margin: 0.07rem 0 0.02rem 0;
-    font-size: .18rem;
+  border-top: 0.01rem solid #f1f1f1;
+
+  &__item {
+    flex: 1;
+    text-align: center;
+    color: $content-font-color;
+    &--active {
+      color: #1fa4fc;
+    }
+    .iconfont {
+      margin: 0.07rem 0 0.02rem 0;
+      font-size: .18rem;
+    }
+  }
+  &__title {
+    font-size: .2rem;
+    transform: scale(0.5,0.5);
+    transform-origin: center top;
   }
 }
-.docker__title {
-  font-size: .2rem;
-  transform: scale(0.5,0.5);
-  transform-origin: center top;
-}
-.docker__item--active {
-  color: #1fa4fc;
-}
+
 </style>
