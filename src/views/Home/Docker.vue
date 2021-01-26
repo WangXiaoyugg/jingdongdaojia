40a365a10dd4372cfde8a52e8a48f8f356931bba
xywang71 <xywang71@iflytek.com>
2021-01-26 21:19:02 +0800

    feat: 首页拆分组件

---
+++ src/views/Home/Docker.vue
@@ -0,0 +1,60 @@
+<template>
+  <div class="docker">
+    <div class="docker__item docker__item--active">
+      <div class="iconfont">&#xe6f3;</div>
+      <div class="docker__title">首页</div>
+    </div>
+    <div class="docker__item">
+      <div class="iconfont">&#xe7e5;</div>
+      <div class="docker__title">购物车</div>
+    </div>
+    <div class="docker__item">
+      <div class="iconfont">&#xe61e;</div>
+      <div class="docker__title">订单</div>
+      </div>
+    <div class="docker__item">
+      <div class="iconfont">&#xe660;</div>
+      <div class="docker__title">我的</div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Docker'
+}
+</script>
+
+<style lang="scss" scoped>
+@import "../../style/variables.scss";
+.docker {
+  display: flex;
+  position: absolute;
+  box-sizing: border-box;
+  padding: 0 .18rem;
+  left: 0;
+  bottom: 0;
+  width: 100%;
+  height: .49rem;
+  border-top: 0.01rem solid $content-bgColor;
+
+  &__item {
+    flex: 1;
+    text-align: center;
+    color: $content-font-color;
+    &--active {
+      color: #1fa4fc;
+    }
+    .iconfont {
+      margin: 0.07rem 0 0.02rem 0;
+      font-size: .18rem;
+    }
+  }
+  &__title {
+    font-size: .2rem;
+    transform: scale(0.5,0.5);
+    transform-origin: center top;
+  }
+}
+
+</style>
---
+++ src/views/Home/StaticPart.vue
@@ -0,0 +1,165 @@
+<template>
+  <div class="position">
+      <span class="iconfont position__icon">&#xe619;</span>
+      北京理工大学国防科技园2号楼10层
+      <span class="iconfont position__notice">&#xe60b;</span>
+    </div>
+    <div class="search">
+      <span class="iconfont">&#xe62d;</span>
+      <span class="search__text">山姆会员商店优惠商品</span>
+    </div>
+    <div class="banner">
+      <img class="banner__img" src="http://www.dell-lee.com/imgs/vue3/banner.jpg" alt="">
+    </div>
+    <div class="icons">
+             <div class="icons__item">
+          <img
+            class="icons__item__img"
+            src="http://www.dell-lee.com/imgs/vue3/超市.png"
+          />
+          <p class="icons__item__desc">超市便利</p>
+        </div>
+        <div class="icons__item">
+          <img
+            class="icons__item__img"
+            src="http://www.dell-lee.com/imgs/vue3/菜市场.png"
+          />
+          <p class="icons__item__desc">菜市场</p>
+        </div>
+        <div class="icons__item">
+          <img
+            class="icons__item__img"
+            src="http://www.dell-lee.com/imgs/vue3/水果店.png"
+          />
+          <p class="icons__item__desc">水果店</p>
+        </div>
+        <div class="icons__item">
+          <img
+            class="icons__item__img"
+            src="http://www.dell-lee.com/imgs/vue3/鲜花.png"
+          />
+          <p class="icons__item__desc">鲜花绿植</p>
+        </div>
+        <div class="icons__item">
+          <img
+            class="icons__item__img"
+            src="http://www.dell-lee.com/imgs/vue3/医药健康.png"
+          />
+          <p class="icons__item__desc">医药健康</p>
+        </div>
+        <div class="icons__item">
+          <img
+            class="icons__item__img"
+            src="http://www.dell-lee.com/imgs/vue3/家居.png"
+          />
+          <p class="icons__item__desc">家居时尚</p>
+        </div>
+        <div class="icons__item">
+          <img
+            class="icons__item__img"
+            src="http://www.dell-lee.com/imgs/vue3/蛋糕.png"
+          />
+          <p class="icons__item__desc">烘培蛋糕</p>
+        </div>
+        <div class="icons__item">
+          <img
+            class="icons__item__img"
+            src="http://www.dell-lee.com/imgs/vue3/签到.png"
+          />
+          <p class="icons__item__desc">签到</p>
+        </div>
+        <div class="icons__item">
+          <img
+            class="icons__item__img"
+            src="http://www.dell-lee.com/imgs/vue3/大牌免运.png"
+          />
+          <p class="icons__item__desc">大牌免运</p>
+        </div>
+        <div class="icons__item">
+          <img
+            class="icons__item__img"
+            src="http://www.dell-lee.com/imgs/vue3/红包.png"
+          />
+          <p class="icons__item__desc">红包套餐</p>
+        </div>
+    </div>
+    <div class="gap"></div>
+</template>
+
+<script>
+export default {
+  name: 'StaticPart'
+}
+</script>
+
+<style lang="scss" scoped>
+@import "../../style/variables.scss";
+@import '../../style/mixins.scss';
+.position {
+  position: relative;
+  padding: .16rem .24rem .16rem 0;
+  line-height: .22rem;
+  font-size: .16rem;
+  color: $content-font-color;
+  @include ellipsis;
+  .position__icon {
+    position: relative;
+    top: .015rem;
+    font-size: .2rem;
+  }
+  .position__notice {
+    position: absolute;
+    right: 0;
+    top: 0.17rem;
+  }
+}
+.search {
+  margin-bottom: .12rem;
+  line-height: .32rem;
+  background: #F5F5F5;
+  color: #b7b7b7;
+  border-radius: .16rem;
+  .iconfont {
+    display: inline-block;
+    padding: 0 .08rem 0 .16rem;
+    font-size: .16rem;
+  }
+  &__text {
+    display: inline-block;
+    font-size: .14rem;
+  }
+}
+.banner {
+  height: 0;
+  overflow: hidden;
+  padding-bottom: 25.4%;
+  &__img {
+    width: 100%;
+  }
+}
+
+.icons {
+  display: flex;
+  flex-wrap: wrap;
+  margin-top: .16rem;
+  &__item  {
+    width: 20%;
+    &__img {
+      display: block;
+      width: .4rem;
+      height: .4rem;
+      margin: 0 auto;
+    }
+    &__desc {
+      margin: 0.06rem 0 .16rem 0;
+      text-align: center;
+      color: $content-font-color;
+    }
+  }
+}
+.gap {
+  margin: 0 -0.18rem;
+  height: .1rem;
+  background: $content-bgColor;
+}
+</style>
