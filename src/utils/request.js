3e44dc0f2525c70b665c007039afb62a68e700d4
xywang71 <xywang71@iflytek.com>
2021-01-30 18:04:15 +0800

    feat: request支持get请求

--- src/utils/request.js
+++ src/utils/request.js
@@ -1,9 +1,22 @@
 import axios from 'axios'
 const BASE_URL = 'https://www.fastmock.site/mock/ae8e9031947a302fed5f92425995aa19/jd/api/'
+const instance = axios.create({
+  baseURL: BASE_URL,
+  timeout: 10000
+})
+export function get (url, params = {}) {
+  return new Promise((resolve, reject) => {
+    instance.get(url, params).then(resp => {
+      resolve(resp.data)
+    }).catch(err => {
+      reject(err)
+    })
+  })
+}
+
 export function post (url, data = {}) {
   return new Promise((resolve, reject) => {
-    axios.post(url, data, {
-      baseURL: BASE_URL,
+    instance.post(url, data, {
       headers: {
         'Content-Type': 'application/json'
       }
