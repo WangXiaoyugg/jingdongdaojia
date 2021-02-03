178be69d7757a41072ebf4508c51924ea548ceab
xywang71 <xywang71@iflytek.com>
2021-02-03 09:53:16 +0800

    feat: 订单页面布局和开发

--- src/views/Shop/Cart.vue
+++ src/views/Shop/Cart.vue
@@ -72,31 +72,13 @@
 </template>
 
 <script>
-import { computed, ref } from 'vue'
+import { ref } from 'vue'
 import { useStore } from 'vuex'
 import { useRoute } from 'vue-router'
 import { useCommonCartEffect } from '../../effects/cartEffects'
 const useCartEffect = (shopId, toggleCartShow) => {
   const store = useStore()
-  const { changeCartItemInfo, productList, cartList } = useCommonCartEffect(shopId)
-  const calculations = computed(() => {
-    const productList = cartList[shopId]?.productList
-    const result = { total: 0, price: 0, allChecked: true }
-    if (productList) {
-      for (const i in productList) {
-        const product = productList[i]
-        result.total += product.count
-        if (product.check) {
-          result.price += (product.count * product.price)
-        }
-        if (product.count > 0 && !product.check) {
-          result.allChecked = false
-        }
-      }
-    }
-    result.price = result.price.toFixed(2)
-    return result
-  })
+  const { changeCartItemInfo, productList, calculations } = useCommonCartEffect(shopId)
 
   const changeCartItemChecked = (shopId, productId) => {
     store.commit('changeCartItemChecked', { shopId, productId })
